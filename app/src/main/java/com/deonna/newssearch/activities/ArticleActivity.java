663368b899fb8bb04d7afa26efbedc5b650fa0d9
Deonna Hodges <deonna@learnersguild.org>
2017-03-17 00:01:05 -0700

    feat(web): Now page is embedded within article activity (instead of opening it up in a separate browser.

--- app/src/main/java/com/deonna/newssearch/activities/ArticleActivity.java
+++ app/src/main/java/com/deonna/newssearch/activities/ArticleActivity.java
@@ -1,8 +1,12 @@
 package com.deonna.newssearch.activities;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.deonna.newssearch.R;
 import com.deonna.newssearch.adapters.ArticlesAdapter;
@@ -23,6 +27,15 @@ public class ArticleActivity extends AppCompatActivity {
 
         String url = getIntent().getStringExtra(ArticlesAdapter.KEY_URL);
 
+        wvArticle.setWebViewClient(new WebViewClient() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+
+                wvArticle.loadUrl(request.getUrl().toString());
+                return true;
+            }
+        });
         wvArticle.loadUrl(url);
     }
 }
