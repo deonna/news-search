dc578d49955d6d433454ae026bfa2d75dcd3403e
Deonna Hodges <deonna@learnersguild.org>
2017-03-19 20:59:06 -0700

    feat(article): Properly formatted date for article views.

--- app/src/main/java/com/deonna/newssearch/models/Article.java
+++ app/src/main/java/com/deonna/newssearch/models/Article.java
@@ -3,7 +3,10 @@ package com.deonna.newssearch.models;
 import com.deonna.newssearch.models.articlesearch.Doc;
 import com.deonna.newssearch.models.articlesearch.QueryResponse;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
@@ -17,6 +20,7 @@ public class Article {
     public String section;
     public String snippet;
     public String publicationDate;
+    public String formattedPublicationDate;
 
     public Article(Doc doc) {
 
@@ -25,12 +29,25 @@ public class Article {
         section = doc.sectionName;
         snippet = doc.snippet;
         publicationDate = doc.pubDate;
+        formattedPublicationDate = getFormattedPublicationDate(publicationDate);
 
         if (!doc.multimedia.isEmpty()) {
             //thumbnail = "http://www.nytimes.com/" + doc.multimedia.get(0).url;
 
             thumbnail = String.format(Locale.US, URL_PREFIX, doc.multimedia.get(0).url);
         }
+    }
+
+    public static String getFormattedPublicationDate(String dateToParse) {
+        Date date = null;
+        try {
+            date = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ssZ").parse(dateToParse);
+             return new SimpleDateFormat("M/dd/yyyy").format(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return "";
     }
 
     public static List<Article> fromQueryResponse(QueryResponse queryResponse)  {
